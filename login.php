5729a51938781e110ba47ecc200f3375590ccf6d
pknight56 <43476014+pknight56@users.noreply.github.com>
2019-05-01 13:25:41 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+       if(($_POST['username'] == "user1") && ($_POST['pass'] == "pass1"))
+
+    { 
+
+     header("Location:http://www.yahoo.com");
+
+    }
+
+
+    else
+
+
+    {
+      echo ("incorrect");
+
+    }
+
+
+    ?>
